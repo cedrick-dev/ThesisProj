14449a2205fb8d130da12e69aff59407ebdcd48b
neveront <memberty.45@gmail.com>
2026-03-26 01:51:25 +0800

    fixed the landscaped qr scanner

---
+++ app/src/main/java/com/mansourappdevelopment/androidapp/kidsafe/activities/CustomScannerActivity.java
@@ -0,0 +1,6 @@
+package com.mansourappdevelopment.androidapp.kidsafe.activities;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CustomScannerActivity extends CaptureActivity {
+}
